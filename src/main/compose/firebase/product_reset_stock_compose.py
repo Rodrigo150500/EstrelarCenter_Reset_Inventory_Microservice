6598251436e0ae5b21d8835c92cdd052051b9f8f
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-19 18:22:12 -0300

    Feat: Add Firebase product reset stock composition function

---
+++ src/main/compose/firebase/product_reset_stock_compose.py
@@ -0,0 +1,12 @@
+from src.model.database.firebase.settings.firebase_db_connection import firebase_db_connection
+from src.model.database.firebase.repositories.product_repository import ProductRepository
+from src.use_case.firebase.reset_stock_product_use_case import ResetStockProductUseCase
+
+
+def product_reset_stock_firebase_compose():
+
+    connection = firebase_db_connection.get_connection()
+    repository = ProductRepository(connection)
+    use_case = ResetStockProductUseCase(repository)
+
+    return use_case
