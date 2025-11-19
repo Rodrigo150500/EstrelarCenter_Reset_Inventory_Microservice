816c0d8146ade25aa191f5285270c65e10e3c9fc
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-19 18:22:19 -0300

    Feat: Refactor product reset stock route to use specific MongoDB and Firebase use cases

--- src/main/routes/product_route.py
+++ src/main/routes/product_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from src.main.compose.mongo_db.product_reset_stock_compose import product_reset_stock_compose
+from src.main.compose.mongo_db.product_reset_stock_compose import product_reset_stock_mongo_db_compose
+from src.main.compose.firebase.product_reset_stock_compose import product_reset_stock_firebase_compose
 from src.errors.error_handler import error_handler
 
 product_route_bp = Blueprint("product_route_bp", __name__)
@@ -9,11 +10,18 @@ def reset_product_stock():
 
     try:
 
-        use_case = product_reset_stock_compose()
-        
-        response = use_case.handle()
+        use_case_mongo_db = product_reset_stock_mongo_db_compose()
+        use_case_firebase = product_reset_stock_firebase_compose()
 
-        return jsonify(response.body), response.status_code
+        response_mongo_db = use_case_mongo_db.handle()
+        response_firebase = use_case_firebase.handle()
+
+        body = {
+            "mongo_db": response_mongo_db.body,
+            "firebase": response_firebase.body
+        }
+
+        return jsonify(body), response_mongo_db.status_code
 
     except Exception as exception:
 
