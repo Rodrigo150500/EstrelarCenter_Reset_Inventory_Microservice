a60503c875f7581e37da6e0a7465ac4af2ff2815
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-21 08:53:53 -0300

    Feat: Update server routes to use specific blueprints for MongoDB and Firebase

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from src.model.database.mongodb.settings.mongo_db_connection import mongo_db_connection
+from src.model.database.firebase.settings.firebase_db_connection import firebase_db_connection
 
-from src.main.routes.mongo_db.product_route import product_route_bp
+from src.main.routes.mongo_db.product_route import product_route_mongo_db_bp
+from src.main.routes.firebase.product_route import product_route_firebase_bp
 
 mongo_db_connection.connect()
+firebase_db_connection.connect()
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(product_route_bp)
+app.register_blueprint(product_route_mongo_db_bp)
+app.register_blueprint(product_route_firebase_bp)
